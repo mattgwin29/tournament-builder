6fd723a8abbd442925e59bc40fed2056157eb969
mattgwin29 <mattgwin@udel.edu>
2023-10-31 16:38:15 -0400

    update latest source code and create bracket dynamically

--- js/extra_bracket.js
+++ js/extra_bracket.js
@@ -1,5 +1,7 @@
 //https://codepen.io/meriawep/pen/ExaqNvd
 
+var dataTemplate = {teams: [], results: []}
+
 var matchData = {
     teams : [
       ["Team 1", "Team 2"],
@@ -76,17 +78,16 @@ var matchData = {
           <p><input class="w3-input w3-padding-16 w3-border" id="tourney_people_count" type="number" placeholder="How many people" required name="People"></p>
           <p><input class="w3-input w3-padding-16 w3-border" type="datetime-local" placeholder="Date and time" required name="date" ></p>
           <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Message \ Special requirements" required name="Message"></p>
-          <p><button class="w3-button w3-black" onclick="render_gracket(10)">Create Event</button></p>
         </form>
       </div>
-      <button onclick="createInputForNames($('#tourney_people_count').val());">Test the bracket designer</button>
-      <button onclick="loadBracket()">Show Test Bracket</button>
+      <p><button class="w3-button w3-black" onclick="createInputForNames();">Next</button></p>
     </div>`);
     $("#current_events").hide()
     
     }
 
-    function createInputForNames(amount){
+    function createInputForNames(){
+        let amount = $('#tourney_people_count').val();
         if ((amount/2) % 2 !=0) {
           alert("The amount of people must be a multiple of 2.");
           return;
@@ -95,22 +96,27 @@ var matchData = {
         document.getElementById('bracket_creator').innerHTML += (`<div id="names_list"></div>`);
     
         let htmlstring = ``;
-        htmlstring+=`<h5 class="w3-center w3-padding-48"><span class="w3-tag w3-wide">Name and Seed </span></h5>`;
+        htmlstring+=`<h5 class="w3-center w3-padding-48"><span class="w3-tag w3-wide">Name and Email</span></h5>`;
         for (var i = 0; i < amount; i++){
-            htmlstring += `<div><p><input type="text" placeholder="Name" id="Name_` + i + `" ><input  type="number" placeholder="Seed"  id="Seed_` + i + `"></p>
+            htmlstring += `<div><p class="w3-center"><input type="text" placeholder="Name" id="Name_` + i + `"><input type="email" placeholder="Email"  id="Email_` + i + `"></p>
           `;
           }
           $("#Seed_" + i).val(i);
-        
+        htmlstring += `<button class="w3-button w3-black" onclick="loadBracket()">Generate Bracket</button>`;
+
         document.getElementById('names_list').innerHTML = htmlstring;
     
         for (var j = 0; j < amount; j++){
           $("#Name_" + j).val("Test" + j);
     
-          $("#Seed_" + j).val(j+1);
+          $("#Email_" + j).val("test@website.com");
         }
       }
-  function loadBracket(){
+
+    
+function loadBracket(){
+    clearBracket();
+    let amount = $('#tourney_people_count').val();
     console.log("Got in here?");
     $("#bracket_holder").html(`<span id="matchCallback"></span>
     <div id="matches">
@@ -122,10 +128,32 @@ var matchData = {
       </div>
     </div>`);
 
+    data = {...dataTemplate};
+    var count = 0;
+    for (var j = 0; j < amount/2; j++){
+        var teamX = []
+        for (var k = 0; k < 2; k++){
+            teamX = [...teamX ,($("#Name_" + (count)).val())];
+            count++;
+        }
+        data.teams = [...data.teams, teamX];
+        //$("#Name_" + j).val("Test" + j);
+        
+        console.log(data.teams);
+        setBracketData(data);
+        //$("#Email_" + j).val("test@website.com");
+    }
 
+  }
+
+  function clearBracket(){
+    setBracketData(dataTemplate);
+  }
+
+  function setBracketData(data){
     $(function() {
         $('#matches .demo').bracket({
-          init: matchData,
+          init: data,
           onMatchClick: onclick,
           onMatchHover: onhover
         })
@@ -138,6 +166,47 @@ var matchData = {
       })
   }
 
+  function getPeopleInTourney(){
+    var items = [];
+    //var item = document.getElementById("tourney_people_count");
+    var amount = $("#tourney_people_count").val();
+    console.log(amount);
+
+    var newArr =[[]];
+
+    console.log("Running the loop until " + amount/2);
+    var count = 0;
+    if (amount > 50){
+      alert("The maximum allowed number of people in a tournament is 50 ");
+      return;
+    }
+    for (var i=1; i<(amount/2)+1; i++){
+      for (var j = 0; j < 2; j++)
+      {
+        var current_name = $("#Name_" + count).val();
+        console.log(current_name);
+        count++;
+      } 
+    }
+  }
+
+  function createRounds(){
+    let htmlstring = ``;
+    let numRounds = $('#tourney_people_count').val();
+    for (var i = 1; i <= getBaseLog(2, numRounds); i++){
+        htmlstring += `<h5 class="w3-center w3-padding-48"><span class="w3-tag w3-wide">Round ` + i + ` </span></h5>`;
+    }
+    $("#round_holder").html(htmlstring);
+}
+
+  /*function createRandomBracket(){
+
+  }*/
+
+
+  function getBaseLog(base, y) {
+    return Math.log(y) / Math.log(base);
+  }
 
   function onclick(data) {
     $('#matchCallback').text("onclick(data: '" + data + "')")
